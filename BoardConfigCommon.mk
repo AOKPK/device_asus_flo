bd619c0030d7c6ac6ec4586c3521a813c9d59e3d
kecinzer <kecinzer@gmail.com>
2014-07-23 13:51:51 +0200

    Use 4.9.2 toolchain for kernel
    As 4.10 is buggy right now
    
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/asus/flo
 TARGET_KERNEL_CONFIG := franco_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.10
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.9
 
 # Optimization
 OPT_A_LOT := true
